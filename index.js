7d1846c01cc9d0562476f08004eb4c6678cfdec3
Anghelo <SV43828468@idat.edu.pe>
2021-09-06 23:04:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//Ejercicio 01
+
+for(var i = 0; i<=20; i++)
+{
+        if(i % 2 == 0)
+        {
+           console.log(i +" es par ");
+        }
+        else
+        {
+           console.log(i +" es impar ");
+        }
+        
+        
+}
+
+// Ejercicio 02
+
+for(var i = 0; i<=10; i++)
+{
+       console.log(i + " x " + "9 es: "+ i*9);
+        
+}
+
+
+//Ejercicio 03
+
+for(var i = 0; i<=10; i++)
+{
+    for(var j = 0;j<=10; j++)
+        {
+            console.log("El resultado de: "+ i +" x " +  j + " es: " + i*j);
+        }
+}
